7d2e3949f972ece97ee75dab3afead03bd70c4a6
Mitchell Gould <mitchellgould7@gmail.com>
2022-01-11 16:30:33 -0500

    animate the menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,12 +20,42 @@ const Header = () => {
           <Logo />
         </LogoWrapper>
         <DesktopNav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
+          <NavLink href="/sale">
+            <NavContent>
+              Sale
+              <span>Sale</span>
+            </NavContent>
+          </NavLink>
+          <NavLink href="/new">
+            <NavContent>
+              New&nbsp;Releases
+              <span>New&nbsp;Releases</span>
+            </NavContent>
+          </NavLink>
+          <NavLink href="/men">
+            <NavContent>
+              Men
+              <span>Men</span>
+            </NavContent>
+          </NavLink>
+          <NavLink href="/women">
+           <NavContent>
+              Women
+              <span>Women</span>
+            </NavContent>
+          </NavLink>
+          <NavLink href="/kids">
+            <NavContent>
+              Kids
+              <span>Kids</span>
+            </NavContent>
+            </NavLink>
+          <NavLink href="/collections">
+            <NavContent>
+              Collections
+              <span>Collections</span>
+            </NavContent>
+          </NavLink>
         </DesktopNav>
         <MobileActions>
           <ShoppingBagButton>
@@ -75,7 +105,6 @@ const DesktopNav = styled.nav`
   display: flex;
   gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
-
   @media ${QUERIES.tabletAndSmaller} {
     display: none;
   }
@@ -114,16 +143,38 @@ const Filler = styled.div`
   }
 `;
 
+const NavWrapper = styled.div`
+
+`;
+
+
 const NavLink = styled.a`
+  --timing-in: 250ms;
+  --timing-out: calc(2 * var(--timing-in));
   font-size: 1.125rem;
+  display: block;
+  height: 25px;
+  overflow: hidden;
   text-transform: uppercase;
   text-decoration: none;
   color: var(--color-gray-900);
   font-weight: ${WEIGHTS.medium};
-
+  &:hover > :first-child {
+    transform: translateY(-50%);
+    transition: var(--timing-in);
+  }
   &:first-of-type {
     color: var(--color-secondary);
   }
 `;
+
+const NavContent = styled.div`
+  transform: translateY(0%);
+  transition: transform var(--timing-out);
+  & > span {
+    display: block;
+    font-weight: ${WEIGHTS.bold};
+  }
+  `;
 
 export default Header;
